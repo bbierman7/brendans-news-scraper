311df961741183f0106b893efdfdea42913be2db
bbierman <bbierman7@gmail.com>
2019-04-26 20:23:25 -0500

    readding { useNewUrlParser: true } to mongoose connection

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/foxsoccer-news-scraper";
-mongoose.connect(MONGODB_URI);
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
